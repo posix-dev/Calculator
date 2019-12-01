46fbcd0f6dfe7ab97da5bd1be0a2528b32c1aff8
pashko00710 <pashko00710@yandex.ru>
2019-12-01 18:01:05 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Pavel Arkhipov on 01.12.2019.
+//  Copyright © 2019 uptop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
